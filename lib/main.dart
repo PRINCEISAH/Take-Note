6e314a7b76eca51f85201d2ffcfe937511b57334
isah <princeisah15@gmail.com>
2020-10-14 15:27:23 -0400

    build runner

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,11 @@ import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:todo/HomePage/HomeScreen.dart';
 import 'package:todo/provider/note_changeNotifier.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final dir = await getApplicationDocumentsDirectory();
+  Hive.init(dir.path);
 
   runApp(
     ChangeNotifierProvider(
